f6581bff05c35c86e6ee7692294ef5e282725dce
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-07-20 16:47:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
 
+
+
 function redirectToPlay() {
    window.location.href = "play.html";
 }
+
+function getRandomWord() {
+  fetch('words.json')
+   .then(response => response.json())
+   .then(words => {
+      const randomWord = words[Math.floor(Math.random() * words.length)];
+      return randomWord;
+    })
+   .then(randomWord => {
+      const wordElement = document.getElementById("random_word");
+      wordElement.textContent = `Random word: ${randomWord}`;
+    })
+   .catch(error => console.error('Error:', error));
+}
+
